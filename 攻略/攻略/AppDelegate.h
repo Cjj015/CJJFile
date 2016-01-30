aa9017046f428b26970aa356899107e906b5cced
蔡 <1330894802@qq.com>
2016-01-30 09:36:08 +0800

    123

---
+++ 攻略/攻略/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  攻略
+//
+//  Created by 蔡 on 16/1/27.
+//  Copyright © 2016年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
